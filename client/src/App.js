e213fc490c40240bb5027b597930adf9dd079b96
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 13:45:54 -0500

    #10 | boilerplate for course

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
+import Courses from "./pages/Courses/Courses";
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Courses />
     </div>
   );
 }
