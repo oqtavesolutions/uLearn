bd57781a72e8ce1531aad898daf49d039ff42abc
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 14:10:24 -0500

    #9 | added explore page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Courses from "./pages/Courses/Courses";
+import Explore from "./pages/Explore/Explore";
 
 function App() {
   return (
     <div className='App'>
       <Header />
       <Courses />
+      <Explore />
     </div>
   );
 }
---
+++ client/src/pages/Explore/Explore.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Explore.scss";
+
+function Explore() {
+  return (
+    <div>
+      Explore
+      <p>categories available</p>
+      <p>title of the course</p>
+      <p>description of the course</p>
+      <p>thumbnail of the course</p>
+      <p>Wrap this up in a link tag</p>
+    </div>
+  );
+}
+
+export default Explore;
