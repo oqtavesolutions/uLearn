6450ce2b6b4be87a235f69220876ff3d867d385c
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 18:06:44 -0500

    #17 | all items needed for header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -6,6 +6,11 @@ function Header() {
   return (
     <div>
       <img src={logo} alt='logo' className='header__logo' />
+      <p>Login</p>
+      <p>Signup</p>
+      <p>Dashboard</p>
+      <p>Notifications</p>
+      <p>User Profile</p>
     </div>
   );
 }
