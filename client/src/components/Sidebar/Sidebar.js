5c04aa77dc2f00a34155c07dad353896711d4f41
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 17:53:44 -0500

    #18 | added sidebar

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Sidebar.scss";
+
+function Sidebar() {
+  return (
+    <div>
+      <p>icon - link</p>
+      <p>icon - link</p>
+      <p>icon - link</p>
+      <p>icon - link</p>
+      <p>icon - link</p>
+    </div>
+  );
+}
+
+export default Sidebar;
