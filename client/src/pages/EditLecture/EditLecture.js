9a5f4aa3008b165450aa943655621543e0fa7658
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-03 14:55:49 -0500

    #46 | added lecture edit page

--- client/src/pages/EditLecture/EditLecture.js
+++ client/src/pages/EditLecture/EditLecture.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./EditLecture.scss";
 
 function EditLecture() {
   return (
-    <div>
-      <p>Title</p>
-      <p>Description</p>
-      <p>Slug</p>
-      <p>Attachment</p>
+    <div className='edit-course-details-form-container'>
+      <h1 className='edit-course-details-form-container__title'>
+        Editing Course
+      </h1>
+      <form className='edit-course-details-form'>
+        <input
+          type='text'
+          placeholder='Course Title'
+          className='edit-course-details-form__input'
+        />
+        <textarea
+          type='text'
+          placeholder='Course Description'
+          className='edit-course-details-form__text-area'></textarea>
+        <input
+          type='text'
+          placeholder='Course Slug'
+          className='edit-course-details-form__input'
+        />
+        <div className='edit-course-details-form__buttons'>
+          <button className='edit-course-details-form__button'>Save</button>
+          <Link className='edit-course-details-form__button edit-course-details-form__button--cancel'>
+            Cancel
+          </Link>
+        </div>
+      </form>
     </div>
   );
 }
