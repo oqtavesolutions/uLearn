0f35c9e51f7cfe5baf87f9c5232172f2ab1cfeb8
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 19:53:14 -0500

    #35 | styled header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import logo from "../../assets/logo/logo.svg";
 import "./Header.scss";
 
 function Header() {
+  const [showMenus, setShowMenus] = useState(false);
+
+  const handleClick = () => {
+    showMenus ? setShowMenus(false) : setShowMenus(true);
+  };
   return (
     <header className='header'>
       <div className='header__logo'>
         <img src={logo} alt='logo' className='header__logo-image' />
       </div>
-      <nav className='header-menus'>
-        <p>Login</p>
-        <p>Signup</p>
-        <p>Dashboard</p>
-        <p>Notifications</p>
-        <p>User Profile</p>
-      </nav>
+      <div className='header-menu-bar'>
+        <FontAwesomeIcon
+          icon={showMenus ? faTimesCircle : faBars}
+          className='header-menu-bar__nav-bar-icon'
+          onClick={handleClick}
+        />
+      </div>
+      {showMenus && (
+        <nav className='header-menus'>
+          <ul className='header-menus__list'>
+            <li className='header-menus__list-item'>Login</li>
+            <li className='header-menus__list-item'>Signup</li>
+            <li className='header-menus__list-item'>Dashboard</li>
+            <li className='header-menus__list-item'>Notifications</li>
+            <li className='header-menus__list-item'>User Profile</li>
+          </ul>
+        </nav>
+      )}
     </header>
   );
 }
