2d2e2a6471d6f5746e33df218582ff56e0f65f6a
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 23:14:20 -0500

    #35 | desktop header works now

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,6 +4,8 @@ import { faBars, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 import { useMediaQuery } from "react-responsive";
 import logo from "../../assets/logo/logo.svg";
 import "./Header.scss";
+import { Link } from "react-router-dom";
+import Avatar from "react-avatar";
 
 function Header() {
   const isDesktop = useMediaQuery({
@@ -11,10 +13,18 @@ function Header() {
   });
 
   const [showMenus, setShowMenus] = useState(false);
+  const [showDesktopCollapsible, setShowDesktopCollapsible] = useState(false);
 
-  const handleClick = () => {
+  const handleNavBarClick = () => {
     showMenus ? setShowMenus(false) : setShowMenus(true);
   };
+
+  const handleCollapsibleClick = () => {
+    showDesktopCollapsible
+      ? setShowDesktopCollapsible(false)
+      : setShowDesktopCollapsible(true);
+  };
+
   return (
     <header className='header'>
       <div className='header__logo'>
@@ -25,19 +35,48 @@ function Header() {
           <FontAwesomeIcon
             icon={showMenus ? faTimesCircle : faBars}
             className='header-menu-bar__nav-bar-icon'
-            onClick={handleClick}
+            onClick={handleNavBarClick}
           />
         </div>
       )}
       {!isDesktop && showMenus && (
-        <nav className='header-menus'>
-          <ul className='header-menus__list'>
-            <li className='header-menus__list-item'>Login</li>
-            <li className='header-menus__list-item'>Signup</li>
-            <li className='header-menus__list-item'>Dashboard</li>
-            <li className='header-menus__list-item'>Notifications</li>
-            <li className='header-menus__list-item'>User Profile</li>
+        <nav className='header-menus-kebab'>
+          <ul className='header-menus-kebab__list'>
+            <li className='header-menus-kebab__list-item'>Login</li>
+            <li className='header-menus-kebab__list-item'>Signup</li>
+            <li className='header-menus-kebab__list-item'>Dashboard</li>
+            <li className='header-menus-kebab__list-item'>Notifications</li>
+            <li className='header-menus-kebab__list-item'>User Profile</li>
           </ul>
+        </nav>
+      )}
+
+      {isDesktop && (
+        <nav className='header-menus'>
+          <div className='header-menus__buttons'>
+            <Link to='/dashboard' className='header-menus__dashboard'>
+              Dashboard
+            </Link>
+          </div>
+          <div className='header-menus__collapse'>
+            <Avatar
+              name='Nahid Hossain'
+              size='50'
+              className='header-menus__avatar'
+              onClick={handleCollapsibleClick}
+              textSizeRatio={1.75}
+              color='#3bc371'
+              fgColor='white'
+            />
+          </div>
+          {showDesktopCollapsible && (
+            <ul className='header-menus__items'>
+              <li className='header-menus__item'>Nahid Hossain</li>
+              <li className='header-menus__item'>oikantik@gmail.com</li>
+              <li className='header-menus__item'>My Account</li>
+              <li className='header-menus__item'>Logout</li>
+            </ul>
+          )}
         </nav>
       )}
     </header>
