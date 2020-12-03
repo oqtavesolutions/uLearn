a8c0d82071596903b1000e5141c495f0340bf4ce
Nahid Hossain <nahid@Nahids-MacBook-Pro.local>
2020-12-02 22:17:47 -0500

    #35 | added react response lib

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "react-responsive";
 import logo from "../../assets/logo/logo.svg";
 import "./Header.scss";
 
 function Header() {
+  const isDesktop = useMediaQuery({
+    query: "(min-device-width: 1280px)",
+  });
+
   const [showMenus, setShowMenus] = useState(false);
 
   const handleClick = () => {
@@ -15,14 +20,16 @@ function Header() {
       <div className='header__logo'>
         <img src={logo} alt='logo' className='header__logo-image' />
       </div>
-      <div className='header-menu-bar'>
-        <FontAwesomeIcon
-          icon={showMenus ? faTimesCircle : faBars}
-          className='header-menu-bar__nav-bar-icon'
-          onClick={handleClick}
-        />
-      </div>
-      {showMenus && (
+      {!isDesktop && (
+        <div className='header-menu-bar'>
+          <FontAwesomeIcon
+            icon={showMenus ? faTimesCircle : faBars}
+            className='header-menu-bar__nav-bar-icon'
+            onClick={handleClick}
+          />
+        </div>
+      )}
+      {!isDesktop && showMenus && (
         <nav className='header-menus'>
           <ul className='header-menus__list'>
             <li className='header-menus__list-item'>Login</li>
